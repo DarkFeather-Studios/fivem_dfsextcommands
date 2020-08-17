79a1e8d443600c43a8a813995d8e1bb19292f317
Daemon Brooks <54357300+xotikorukx@users.noreply.github.com>
2020-08-17 14:44:27 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,8 +7,11 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 dependencies {
 
     'dfs',
-    'progressBars',
-    'mythic_notify'
+    'dfsdb_perms',
+    'dfsAlerts'
+    --'progressBars',
+    --'mythic_notify'
+
 }
 
 server_script 'dfs_extcommands_sv.lua'
